475b9436ecdb647ada80b9192c7a0f775ad6578f
afcarioca <afcarioca@hotmail.com>
2020-12-08 20:56:18 +0000

    Add Routes and Token

---
+++ src/Componentes/Login/Login.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+
+export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            token : ''
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+
+    async handleSubmit(event){ 
+        event.preventDefault();
+        await fetch('http://localhost:8000/api/login', {
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+        "email": this.email.value,
+        "password": this.password.value,
+         })
+        }).then(res => res.json())
+        .then(json => 
+             this.setState({token: json.token})
+        ).catch(error =>
+             console.log(error.response)
+        )
+        
+        console.log(this.state.token);    
+        if(this.state.token != undefined){
+            this.props.history.push('/noticias');
+        }
+            
+       
+
+      
+     
+
+    };
+
+   
+
+     render(){
+
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <input ref={(ref) => {this.email = ref}} placeholder="Usuário" type="email" name="email"/><br />
+                <input ref={(ref) => {this.password = ref}} placeholder="Senha" type="password" name="password"/><br />
+               
+               <input type="submit" value="Submit" />
+            </form>
+        );
+      }
+}
+
+
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ShowNoticia from './Componentes/Noticias/Show/ShowNoticia';
 import StoreNoticia from './Componentes/Noticias/Store/StoreNoticia';
 import UpdateNoticia from './Componentes/Noticias/Update/UpdateNoticia';
 import DeleteNoticia from './Componentes/Noticias/Delete/DeleteNoticia';
+import Login from './Componentes/Login/Login';
 
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
@@ -18,6 +19,8 @@ ReactDOM.render(
       <Route path="/noticia"  exact={true}  component={StoreNoticia}/>
       <Route path="/update/noticia/:id"  exact={true}  component={UpdateNoticia}/>
       <Route path="/delete/noticia/:id" exact={true} component={DeleteNoticia} />
+      <Route path="/Login" exact={true} component={Login} />
+
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
